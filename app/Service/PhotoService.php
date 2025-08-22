67c6bc9259d0f6c71bf9c409f03074d126f29ca8
= <=>
2025-08-22 08:01:10 +0700

    menyambungkan branch EditDatabase dengan Master

---
+++ app/Service/PhotoService.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Http\Request;
+
+class PhotoService{
+    protected $media;
+
+    public function __construct(Request $media) {
+        $this->$media = $media;
+    }
+
+    public function uploadImage(String $inputFile){
+        if(!$this->media->hasFile($inputFile)){
+
+        }
+        return $this->media->file("")->store("images","public");
+    }
+}
